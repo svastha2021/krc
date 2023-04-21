50cea2b122aa72b073633edfaf94343ee0ca8b28
ArunEj69 <arundes69@gmail.com>
2023-04-21 07:51:48 +0530

    eod and print issue changes.

--- src/app/eod/eod.component.ts
+++ src/app/eod/eod.component.ts
@@ -1,19 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { EodService } from './eod.service';
-import { MatDialog } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { InfoDialogComponent } from '../utilities/info-dialog/info-dialog.component';
 import { Router } from '@angular/router';
 import { DatePipe } from '@angular/common';
 import { UtilityService } from '../utilities/services/utility.service';
 declare var $:any
 
+export interface DialogData {
+  eodData: any;
+  setDate: any;
+  newEod: any;
+  currDate: any;
+  newEodStr: any;
+}
 @Component({
   selector: 'app-eod',
   templateUrl: './eod.component.html',
   styleUrls: ['./eod.component.scss'],
   providers: [DatePipe]
 })
+
 export class EodComponent implements OnInit {
   orgId: any;
   branchId: any;
@@ -23,8 +31,9 @@ export class EodComponent implements OnInit {
   newEod: any;
   currDate: any;
   newEodStr: any;
+  
 
-  constructor(private eodService: EodService, private formBuilder: FormBuilder,private dialog: MatDialog, private router: Router, 
+  constructor(private eodService: EodService, private formBuilder: FormBuilder, private dialog: MatDialog, private router: Router, 
     private dp: DatePipe, private us: UtilityService) { }
 
   ngOnInit(): void {
@@ -60,7 +69,6 @@ export class EodComponent implements OnInit {
     this.setDate = this.newEodStr;
     let todayDate = (new Date()).getTime();
     let systemNewDate = (new Date(this.setDate)).getTime();
-    // console.log(systemNewDate);
     if(todayDate == systemNewDate || todayDate < systemNewDate){
       this.dialog.open(InfoDialogComponent, {
         width: '400px',
@@ -68,36 +76,110 @@ export class EodComponent implements OnInit {
       })
       return;
     }
-    this.setNewEodDate()
-    $("#MyPopup").modal("show");
+    this.setNewEodDate();
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      width: '400px',
+      data: {currDate: this.currDate, newEodStr: this.newEodStr, newEod: this.newEod}
+    });
+    // $("#MyPopup").modal("show");
   }
 
-  openModal1() {
-    $("#MyPopup").modal("hide");
-    $("#MyPopup1").modal("show");
-  }
+  // openModal1() {
+  //   $("#MyPopup").modal("hide");
+  //   $("#MyPopup1").modal("show");
+  // }
 
-  close() {
-    $("#MyPopup").modal("hide");
-  }
+  // close() {
+  //   $("#MyPopup").modal("hide");
+  // }
 
-  close1() {
-    $("#MyPopup1").modal("hide");
-  }
+  // close1() {
+  //   $("#MyPopup1").modal("hide");
+  // }
 
   setNewEodDate() {
     let date = new Date(this.currDate);
     this.newEod = date.setDate(date.getDate() + 1);
     this.newEodStr = this.us.convertTodayTostr(date)
     console.log(this.us.convertTodayTostr(date))
-    console.log("final---");
+  }
+
+  // closeModal() {
+  //   $("#MyPopup1").modal("hide");
+  //   let param = {
+  //     org_id: localStorage.getItem('org_id'), branch_id: localStorage.getItem('branch_id'), user_id: localStorage.getItem('user_id'),
+  //     new_eod_date: this.newEodStr, eod_date: this.currDate
+  //   }
+  //   this.eodService.createEod(param).subscribe(data => {
+  //     console.log(data);
+  //     this.dialog.open(InfoDialogComponent, {
+  //       width: '400px',
+  //       data: 'EOD Completed Successfully.  Your Next Business Date : '+ this.us.convertTodayTostrDDMMYYYY(this.newEod)
+  //     })
+  //     this.router.navigate(['/landing'])
+  //   })
+  // }
+}
+
+@Component({
+  selector: 'dialog-model1-dialog',
+  templateUrl: 'dialog-model1-dialog.html',
+})
+export class DialogOverviewExampleDialog implements OnInit {
+  [x: string]: any;
+  currentDate: any
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData, private us: UtilityService, private dialog: MatDialog,
+  ) { }
+  ngOnInit() {
+    this.currentDate = this.us.convertTodayTostrDDMMYYYY(this.data.currDate);
+    this.newEodStr = this.data.newEodStr;
+    this.newEod = this.data.newEod;
+  }
+  
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 
   closeModal() {
-    $("#MyPopup1").modal("hide");
+    this.onNoClick();
+    const dialogRef = this.dialog.open(DialogOverviewModal1, {
+      width: '400px',
+      data: {currDate: this.data.currDate, newEodStr: this.newEodStr, newEod: this.newEod}
+    });
+  }
+
+
+}
+
+@Component({
+  selector: 'dialog-model2-dialog',
+  templateUrl: 'dialog-model2-dialog.html',
+})
+export class DialogOverviewModal1 implements OnInit {
+  [x: string]: any;
+  currentDate1: any;
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewModal1>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData, private us: UtilityService, private dialog: MatDialog,
+    private eodService: EodService, private router: Router
+  ) { }
+  ngOnInit() {
+    this.currentDate1 = this.us.convertTodayTostrDDMMYYYY(this.data.currDate);
+    this.newEodStr = this.data.newEodStr;
+    this.newEod = this.data.newEod;
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  createEod() {
+    this.onNoClick();
     let param = {
       org_id: localStorage.getItem('org_id'), branch_id: localStorage.getItem('branch_id'), user_id: localStorage.getItem('user_id'),
-      new_eod_date: this.newEodStr, eod_date: this.currDate
+      new_eod_date: this.newEodStr, eod_date: this.data.currDate
     }
     this.eodService.createEod(param).subscribe(data => {
       console.log(data);
